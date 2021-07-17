6ea8a2484e6b9e0733e41608e29aa8c13a9137bf
Ahmed Hafez <gm.ahmed.hafez@gmail.com>
2021-07-18 01:42:28 +0200

    defining hospital classes

--- hospital-sys-design.py
+++ hospital-sys-design.py
@@ -1,1 +1,13 @@
 # Hospital system design project from Mastering 4 critical skills using python 
+
+class Patient():
+    def __init__(self,name,status,specilization):
+        self.name = name 
+        self.status = status 
+        self.specializatation = specializatation 
+        
+
+class Specialization():
+    queue = [] # list of patients in the speciliazation 
+    def __init__(self, specialtion_name):
+        self.specialization_name = specialtion_name
